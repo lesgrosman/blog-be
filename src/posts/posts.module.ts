f8fd443d38e6f72de227f794817fb991bf4ff16d
Glib Konstantinov <glibkonstantinov@Glibs-MacBook-Pro.local>
2023-04-17 16:19:59 +0200

    post routes

---
+++ src/posts/posts.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { PostsController } from './posts.controller';
+import { PostsService } from './posts.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Module({
+  controllers: [PostsController],
+  providers: [PrismaService, PostsService],
+})
+export class PostsModule {}
